a165a322c29fc0f46a6145a9fb2a9cdff54abeb6
María <marferpv@gmail.com>
2023-11-24 12:17:07 -0600

    LandPage - Registro - Login
    Estructuras -Falta funcionalidad PHP

---
+++ Registrarse.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+        <!----------------------- HEADER CON EL NAVABAR --------------------->
+        <header>
+<nav id="navbar-example2" class="navbar bg-primary " data-bs-theme="dark">>
+          <a class="navbar-brand" href="LandPage.php" class="nav-link-menu nav-link" LandPage style="font-color">SG GANADERIA</a>
+          <ul class="nav nav-pills">
+                <ul class="navbar-nav me-auto ">
+                <button  class="btn btn-success"  href="login.php" type="submit">Iniciar Sesión</button>
+                </ul>
+        
+                <ul class="navbar-nav me-auto ">
+                <button class="btn btn-success"  href="Registrarse.php" type="submit">Registrarse</button>
+               </ul>
+         
+        </nav>
+</header>
+ <!----------------------- COLUMNAS PARA SEPAR DEL HEADER --------------------->
+<body>
+<div class="container text-center">
+  <div class="row g-2">
+    <div class="col-6">
+      <div class="p-3"></div>
+    </div>
+    <div class="col-6">
+      <div class="p-3"></div>
+    </div>
+  </div>
+</div>
+
+  <!----------------------- COLUMNAS PARA SEPAR DEL HEADER --------------------->
+<body>
+<div class="container text-center">
+  <div class="row g-2">
+    <div class="col-0">
+      <div class="p-6">
+                      <h1 text align= center> ¡únete a Nosotros! </h1>
+                <form class="row g-3">
+                  <div class="col-md-6">
+                    <label for="inputName" class="form-label">Nombre</label>
+                    <input type="email" class="form-control" id="inputName" placeholder="Ingresa tu nombre">
+                  </div>
+                  <div class="col-md-6">
+                    <label for="inputApellido" class="form-label">Apellido</label>
+                    <input type="email" class="form-control" id="inputLastName" placeholder="Ingresa tu apellido">
+                  </div>
+                  <div class="col-6">
+                    <label for="inputEmail4" class="form-label">Email</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Ingresa tu email">
+                  </div>
+                  <div class="col-6">
+                    <label for="inputPassword" class="form-label">Contraseña</label>
+                    <input type="text" class="form-control" id="inputpassword" placeholder="Ingresa la contraseña">
+                  </div>
+                  <div class="col-12">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="gridCheck">
+                      <label class="form-check-label" for="gridCheck">
+                       Acepto todos los terminos de privacidad, ¡Regístrame!
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Terminar Registro</button>
+                  </div>
+                </form>
+                </body>
+      </div>
+    </div>
+    <div class="col-6">
+      <div class="p-3"></div>
+    </div>
+  </div>
+</div>
+ <!----------------------- SCROLL ------------------------------------------>
+ 
+<!-----------------------FOOTER--------------------------------------------->
+<footer>
+<h1 text align= center> Contacto </h1>
+
+</fooder>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
