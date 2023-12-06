05e415b5141560e2414ced385bebfef4d9553592
María <marferpv@gmail.com>
2023-12-05 23:53:21 -0600

    Partos update tabla

--- view/partos/parto_nuevo_registro.php
+++ view/partos/parto_nuevo_registro.php
@@ -8,10 +8,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SG GANADERIA| NUEVO Parto</title>
+  <title>SG GANADERIA | PARTOS</title>
 
-   <!-- Google Font: Source Sans Pro -->
-   <link rel="stylesheet"
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
@@ -19,6 +19,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   <!-- Datatable CSS -->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+    <!-- DataTables -->
+    <link rel="stylesheet" href="../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
   <!-- Toastr -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 
@@ -26,11 +30,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </head>
 
 <body class="hold-transition sidebar-mini">
+
+  <!--  Main Page -->
   <div class="wrapper">
 
+    <!--  Main Navbar -->
     <?php
     include '../fragments/nav_menu.php'
-      ?>
+      ?><!--  Main navbar -->
+
+
 
     <!--  Main Sidebar  -->
     <aside class="main-sidebar  elevation-4">
@@ -40,17 +49,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
     </aside><!--  Main Sidebar  -->
 
 
-    <!-- Main-->
+    <!-- Main Content-->
     <div class="bg-white content-wrapper">
-      <!-- Content Header-->
+
+      <!-- Content Header (Page header) -->
       <div class="content-header">
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-left">
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item "><a href="#">Control de Partos</a></li>
-                <li class="breadcrumb-item Active"><a href="#">Añadir Parto</a></li>
+                <li class="breadcrumb-item"><a href="#">Control de Partos</a></li>
+                <li class="breadcrumb-item active">Añadir Parto</li>
               </ol>
             </div>
             <div class="col-sm-6">
@@ -60,144 +70,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </div><!-- /.container-fluid -->
       </div><!-- /.content-header -->
 
-  
-      <!-- Main content -->
-      <section class="content">
+      <!--  Main section -->
+      <section class="main-content">
         <div class="container-fluid">
-
-          <!-- Formulario Agregar-->
-          <div class="row mb-5" id="form-agregar">
-            <div class="col-2"></div>
-            <div class="col-8">
-              <div class="card " style="background-color: #52AA5E;">
-                <div class="card-header text-center">
-                  <h3 class="card-title text-white">Nuevo Parto</h3>
-                </div>
-                <!-- /.card-header -->
-                <!-- form start -->
-                <form>
-                  <div class="card-body text-white ">
-                    <div class="row">
-                      <div class="col-12">
-                        <div class="form-group">
-                          <label for="">ID de la Vaca</label>
-                          <input type="number" class="form-control" id="" placeholder="Ingrese el id de la vaca">
-                        </div>
-                      </div>
-      
-                      <div class="col-12">
-                        <div class="form-group">
-                          <label for="">Numero de Arete</label>
-                          <input type="number" class="form-control" id="" placeholder="Ingrese el número de arete de la vaca">
-                        </div>
-                      </div>
-                      <div class="col-6">
-                        <div class="form-group">
-                          <label for="fecha_nacimiento">Fecha de parto</label>
-                          <input type="date" class="form-control" id="" placeholder="Ingrese la fecha de parto de la vaca">
-                        </div>
-                      </div>
-                      <div class="col-6">
-                        <div class="form-group">
-                          <label>Tipo de parto</label>
-                          <select class="form-control select2">
-                            <option selected="selected">Natural</option>
-                            <option>Intervenido</option>
-                            <option>Cesárea</option>
-                          </select>
-                        </div>
-                        </div>
-                      <div class="col-12">
-                        <div class="form-group">
-                          <label for="">Observaciones</label>
-                          <input type="text" class="form-control" id=""
-                            placeholder="Ingrese las observaciones correspondientes">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- /.card-body -->
-
-                  <div class="card-footer text-center">
-                    <button type="submit" class="btn btn-primary">Guardar</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <div class="col-2"></div>
-          </div>
-          <!-- /.row -->
-
-          <!-- Formulario Modificar-->
-          <div class="row mb-5" id="form-modificar">
-            <div class="col-2"></div>
-            <div class="col-8">
-              <div class="card " style="background-color: grey;">
-                <div class="card-header text-center">
-                  <h3 class="card-title text-white">Nuevo Parto</h3>
-                </div>
-                <!-- /.card-header -->
-                <!-- form start -->
-                <form>
-                  <div class="card-body text-white ">
-                    <div class="row">
-                      <div class="col-6">
-                        <div class="form-group">
-                          <label for="">ID de la vaca</label>
-                          <input type="number" class="form-control" id="" placeholder="Ingrese el número de ID">
-                        </div>
-                      </div>
-                      <div class="col-6">
-                        <div class="form-group">
-                          <label>Número de Arete</label>
-                          <input type="number" class="form-control" id="" placeholder="Ingrese el número de arete de la vaca">
-                        </div>
-                      </div>
-                      <div class="col-6">
-                        <div class="form-group">
-                          <label for="">Fecha de Parto</label>
-                          <input type="text" class="form-control" id="" placeholder="Ingrese la fecha de parto">
-                        </div>
-                      </div>
-                      <div class="col-6">
-                        <div class="form-group">
-                          <label>Tipo de parto</label>
-                          <select class="form-control select2">
-                            <option selected="selected">Natural</option>
-                            <option>Intervenido</option>
-                            <option>Cesárea</option>
-                          </select>
-                        </div>
-                        </div>
-                        <div class="col-12">
-                        <div class="form-group">
-                          <label for="">Observaciones</label>
-                          <input type="text" class="form-control" id=""
-                            placeholder="Ingrese las observaciones correspondientes">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- /.card-body -->
-
-                  <div class="card-footer text-center">
-                    <button type="submit" class="btn btn-primary">Guardar</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <div class="col-2"></div>
-          </div>
-          <!-- /.row -->
-
           <!-- Tabla -->
-          <div class="row mb-5" id="tabla-vacas">
-            <div class="col-md-12">
+          <div class="row mb-5" id="tabla-enfermedades">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
               <div class="card card-dark">
-                <div class="card-body p-0">
+                <div class="card-body p-3">
                   <table id="tablalistado" class="table table-striped table-bordered table-hover">
                     <thead>
-                      <th>ID de la Vaca</th>
+                    <th>ID de la Vaca</th>
                       <th>Número de Arete</th>
                       <th>Fecha del parto</th>
                       <th>Tipo de parto</th>
@@ -205,23 +89,151 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     </thead>
                     <tbody>
                     </tbody>
-
                   </table>
                 </div>
               </div>
               <!-- /.card -->
             </div>
+
+            <div class="col-md-1"></div>
           </div>
-          <!-- /.row -->
-        </div>
-        <!-- /.container-fluid -->
-      </section>
-      <!-- /.content -->
+          <!-- /.tabla -->
+
+          <!-- Formulario Agregar-->
+          <div class="row mb-5" id="form-agregar">
+            <div class="col-2"></div>
+            <div class="col-8">
+              <div class="card " style="background-color: #28a745;">
+                <div class="card-header text-center">
+                  <h3 class="card-title text-white">Nuevo Parto</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form id="formulario-agregar" method="POST">
+                  <div class="card-body text-white ">
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="">Numero de Arete</label>
+                          <input type="number" class="form-control" name="numeroArete"
+                            placeholder="Ingrese el número del Arete" required>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="">Fecha del Parto</label>
+                          <input type="date" class="form-control" name="fechaParto"
+                            placeholder="Ingrese la fecha del parto" required>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="">Tipo de Parto</label>
+                          <select class="form-control select2">
+                            <option selected="selected">Natural</option>
+                            <option>Intervenido</option>
+                            <option>Cesárea</option>
+                          </select>
+                        </div>
+                      </div>
+
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="">Observaciones</label>
+                          <input type="text" class="form-control" name="observaciones"
+                            placeholder="Ingrese sus Observaciones" required>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.card-body -->
+
+                  <div class="card-footer text-center">
+                    <div class="row">
+                      <div class="col-6"><button type="submit" class="btn btn-primary btnRegistrar">Guardar</button>
+                      </div>
+                      <div class="col-6"><input type="reset" class=" btn btn-info" value="Limpiar datos"></div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-2"></div>
+          </div>
+          <!-- /.fromulario -->
+
+          <!-- Formulario Modificar-->
+          <div class="row mb-5" id="form-modificar">
+            <div class="col-2"></div>
+            <div class="col-8">
+              <div class="card " style="background-color: grey;">
+                <div class="card-header text-center">
+                  <h3 class="card-title text-white">Modificar Parto</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form id="formulario-modificar" method="POST">
+                  <div class="card-body text-white ">
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="form-group">
+                        <label for="">Numero de Arete</label>
+                          <input type="number" class="form-control" name="numeroArete"
+                            placeholder="Ingrese el número del Arete" required>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="">Fecha del Parto</label>
+                          <input type="date" class="form-control" name="fechaParto"
+                            placeholder="Ingrese la fecha del parto" required>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="">Tipo de Parto</label>
+                          <input type="text" class="form-control" name="TipoParto" placeholder="Ingrese los sintomas" required>
+                        </div>
+                      </div>
+
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="">Observaciones</label>
+                          <input type="text" class="form-control" name="observaciones"
+                            placeholder="Ingrese sus Observaciones" required>
+                        </div>
+                      </div>
+                    </div>
+
+
+                  </div>
+                  <!-- /.card-body -->
+
+                  <div class="card-footer text-center">
+                    <div class="row">
+                      <div class="col-6"><button type="submit" class="btn btn-primary">Editar</button></div>
+                      <div class="col-6"><input type="button" class=" btn btn-info" value="Cancelar"
+                          onclick="cancelarForm()"></div>
+                    </div>
+
+
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-2"></div>
+          </div>
+          <!-- /.formulario-->
+
+
+
+        </div><!-- /.container-fluid -->
+      </section><!--main section-->
 
 
 
 
-    </div><!-- ./Content Wrapper-->
+    </div><!-- ./main contentr-->
 
 
 
@@ -230,17 +242,35 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <?php
       include '../fragments/footer.php'
         ?>
-    </footer>
+    </footer><!-- footer -->
 
-  </div><!-- ./wrapper -->
+  </div><!-- ./main oage -->
+
+
+
 
 
 </body>
+
+<!-- REQUIRED SCRIPTS -->
 
 <!--   JQUERY -->
 <script src="../plugins/jquery/jquery.min.js"></script>
 <!-- DataTables  & Plugins -->
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../plugins/jszip/jszip.min.js"></script>
+<script src="../plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
 <!-- Bootstrap 4 -->
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
@@ -254,6 +284,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <link rel="stylesheet" href="../assets/css/index.css">
 
 <!-- JD Scripts -->
-<script src="../assets/js/vacunas.js"></script>
+<script src="../assets/js/partos.js"></script>
+
 
 </html>
